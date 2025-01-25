2f3cf945eacb513c0e0590e4a28f9bfdfc128d77
alanesg <alanesGsilva@gmail.com>
2025-01-24 22:49:09 -0300

    atualizando

--- 1023/index.js
+++ 1023/index.js
@@ -5,17 +5,23 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-rl.question("Digite um número para A: ", (valorA) => {
-    let a = parseFloat(valorA)
+rl.question('Digite três valores de ponto flutuante (double) A, B, C separados por espaço: ', 
+    (input) => {const [A, B, C] = input.split(' ').map(Number);
+     //o input.split é um método em javascript,
+    //  que divide uma string em um array de substrings,
+    //separados por um caractere especificado ["A", "B", "C"]
+     
+    // o A === 0 verifica se A é zero.
+    // o B**2-4*A*C < 0 verifica se o discriminante da equação é negativo.
+    if (A === 0 || B**2 -4*A*C < 0) {
+        console.log('Impossível calcular.');
+    } else {
+        const R1 = (-B + Math.sqrt(B**2 -4*A*C)) / (2*A);
+        const R2 = (-B - Math.sqrt(B**2 -4*A*C)) / (2*A);
 
-    rl.question("Digite um valor para B: ", (valorB) => {
-        let b = parseFloat(valorB)
-
-        rl.question("Digite um valor para C: ", (valorC) => {
-        let c = parseFloat(valorC)
-
+        console.log(`R1 = ${R1.toFixed(5)}`);
+            console.log (`R2 = ${R2.toFixed(5)}`);
+    }
 
             rl.close();
-        })
-    })
-})
+        });
