6a19e34f188f78c6e24c2b0ab8834acc181da9d1
alanesg <alanesGsilva@gmail.com>
2025-01-31 22:18:03 -0300

    concluído

--- 1024/index.js
+++ 1024/index.js
@@ -1,14 +1,31 @@
+//readline ler a entrada do usuário;
 const readline = require('readline');
 
-const rl = readline.CreateInterface ({
+//criando a interface da leitura: 
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
-rl.question("Digite um valor para A", (valorA) => {
-let a = parseInt(valorA)
+// ler o valor:
+rl.question('Digite um valor: ', (numero) => {
+numero = parseFloat(numero);
 
+//verificar em qual dos intervalos esse valor se encontra:
 
-
+if (numero >= 0 && numero <= 25){
+        console.log('Intervalo [0,25]');
+    } else if (numero > 25 && numero <= 50){
+        console.log('Intervalo [25,50]');
+        }else if (numero > 50 && numero <= 75){
+        console.log('Intervalo [50,75]');
+    }else if (numero > 75  && numero <= 100){
+        console.log('Intervalo [75,100]');
+    }else {
+        console.log('Fora de intervalo')
+}
+    
 rl.close();
 })
+
+  
